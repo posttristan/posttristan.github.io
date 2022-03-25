c2ea502fa3247d81f53bbc6ec59781c7a7ae2d29
posttristan <102298652+posttristan@users.noreply.github.com>
2022-03-25 21:45:42 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(function () {
     $('.topBanner i ').on('click', topbannerHandler);
 
 
-
+    //***가각의 슬라이드에 애니메이션~ */
     $('.mainVisualSlide').on('init reInit afterChange', function () {
         let current = $('.slick-current');
         current.addClass('on').siblings().removeClass('on');
@@ -36,6 +36,12 @@ $(function () {
     //     $('.mainVisualSlide figure').eq(c + 1).addClass('on');
     // });
 
+    $('.movieBtn i:first-child').on('click', function () {
+        $('#adMovie').trigger('play');
+    });
+    $('.movieBtn i:last-child').on('click', function () {
+        $('#adMovie').trigger('pause');
+    });
 
 
 
